a6e57934fc14eb8dcca303c2cc4042672ca9cd40
IsshinTheSwordSaint <ut_4003@zsp3zamosc.pl>
2025-11-04 13:26:02 +0100

    Zadanie3

---
+++ Zadanie3/Zadanie3.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+double temperaturaCelsjusz = 25;
+double temperaturaFahrenheit = (temperaturaCelsjusz * 9 / 5) + 32;
+
+temperaturaCelsjusz = (temperaturaFahrenheit - 32) * 5 / 9;
+
+if(temperaturaCelsjusz < 0)
+    {
+    Console.WriteLine("Temperatura poniżej zera!");
+}
+
+Console.WriteLine($"Temperatura w Celsjuszach: {Math.Round(temperaturaCelsjusz, 2)}°C");
+Console.WriteLine($"Temperatura w Fahrenheitach: {Math.Round(temperaturaFahrenheit, 2)}°F");
+
